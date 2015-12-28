0cecd7b72e53b6b408b5f24e5335d61cc786baa2
Colin Newell <colin.newell@gmail.com>
2015-12-28 19:06:17 +0000

    Started to add build info

--- lib/DBIC/Example.pm
+++ lib/DBIC/Example.pm
@@ -2,4 +2,23 @@ use strict;
 use warnings;
 package DBIC::Example;
 
+our $VERSION = '1';
+
 1;
+
+=head1 NAME
+
+DBIC::Example - Playing with DBIx::Class
+
+=head1 DESCRIPTION
+
+=head1 METHODS
+
+=head1 ATTRIBUTES
+
+
+=head1 LICENSE AND COPYRIGHT
+
+Copyright 2015 Colin Newell.
+
+=cut
